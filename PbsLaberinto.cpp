4aeb64711683f3da74d9e44f9a6f356ef1506307
AMargery <38414315+AMargery@users.noreply.github.com>
2018-04-23 08:46:27 -0600

    Update PbsLaberinto.cpp

--- PbsLaberinto.cpp
+++ PbsLaberinto.cpp
@@ -26,53 +26,49 @@ void testLaberinto(int cantidadVrts, double probabilidadAdy) {
     int maxAdy = totalVrts*(totalVrts-1);
     if(totalVrts != cantidadVrts) 
         std::cout << "Falló el constructor estándar" << std::endl;
+    
     if(totalAdy > maxAdy)
         std::cout << "Falló el constructor estándar" << std::endl;
 
 }
 
-void testLaberinto2(ifstream& archivo) {
-    std::cout << "" << std::endl;
+void testLaberinto2() {
+    Laberinto laberintoP("laberintop.txt");
+    Laberinto laberintoM("laberintom.txt");
+    int laberintoPVrt = laberintoP.obtTotVrt();
+    int laberintoMVrt = laberintoM.obtTotVrt();
+    if((laberintoPVrt != 10) || (laberintoPVrt != 50))
+        std::cout << "Fallo el constructor con archivos en cuanto a la cantidad de vertices" << std::endl;
+   
+    int pe;
+    ifstream archivoEnterosEntrada("datos-red.txt", ios::in);
+    char finLinea = ' ';
+
+    if (!archivoEnterosEntrada) { // operador ! sobrecargado
+        cerr << "No se pudo abrir el archivo de entrada" << endl;
+        return 1;
+    }
+    std::cout << "Falló el constructor con archivos en cuanto a las adyacencias de cada vertice" << std::endl;
+    
 }
 
 void testLaberinto3() {
-    std::cout << "" << std::endl;
-}
-
-void testXstVrt() {
-    std::cout << "" << std::endl;
-}
-
-void testXstAdy() {
-    std::cout << "" << std::endl;
-}
-
-void testObtIdVrtInicial() {
-    std::cout << "" << std::endl;
-}
-
-void testObtIdVrtFinal() {
-    std::cout << "" << std::endl;
-}
-
-void testObtIdVrtAdys() {
-    std::cout << "" << std::endl;
-}
-
-void testObtDatoAdy() {
-    std::cout << "" << std::endl;
-}
-
-void testObtCntAdy() {
-    std::cout << "" << std::endl;
-}
-
-void testObtTotAdy() {
-    std::cout << "" << std::endl;
-}
-
-void testObtTotVrt() {
-    std::cout << "" << std::endl;
+    Laberinto laberinto10(10,0.3);
+    Laberinto laberinto100(100,0.3);
+    Laberinto laberinto10C(laberinto10);
+    Laberinto laberinto100C(laberinto100);
+    int vrt10 = laberinto10.obtTotVrt();
+    int vrt10C = laberinto10C.obtTotVrt();
+    int vrt100 = laberinto100.obtTotVrt();
+    int vrt100C = laberinto100C.obtTotVrt();
+    int ady10 = laberinto10.obtTotAdy();
+    int ady10C = laberinto10C.obtTotAdy();
+    int ady100 = laberinto100.obtTotAdy();
+    int ady100C = laberinto100C.obtTotAdy();
+    if((vrt10 != vrt10C) || (vrt100 != vrt100C))
+        std::cout << "Falló el constructor de copias en cuanto a la cantidad de vertices" << std::endl;
+    if((ady10 != ady10C) || (ady100 != ady100C))
+         std::cout << "Falló el constructor de copias en cuanto a la cantidad de adyacencias totales" << std::endl;
 }
 
 void testCaminoMasCorto() {
@@ -87,32 +83,8 @@ void testSumaTotalFerormona() {
     std::cout << "" << std::endl;
 }
 
-void testAsgIdVrtInicial() {
-    std::cout << "" << std::endl;
-}
-
-void testAsgIdVrtFinal() {
-    std::cout << "" << std::endl;
-}
-
-void testAsgDatoAdy() {
-    std::cout << "" << std::endl;
-}
-
-void testDecrementarFerormonaAdys() {
-    std::cout << "" << std::endl;
-}
-
-void testActualizarValoracionAdys() {
-    std::cout << "" << std::endl;
-}
-
-void testObtIndiceAdy() {
-    std::cout << "" << std::endl;
-}
-
 int main(int argc, char** argv) {
-    std::cout << "%SUITE_STARTING% newsimpletest" << std::endl;
+    std::cout << "%SUITE_STARTING% PbsLaberinto" << std::endl;
     std::cout << "%SUITE_STARTED%" << std::endl;
 
     std::cout << "%TEST_STARTED% testLaberinto (PbsLaberinto)" << std::endl;
@@ -126,43 +98,7 @@ int main(int argc, char** argv) {
     std::cout << "%TEST_STARTED% testLaberinto3 (PbsLaberinto)" << std::endl;
     testLaberinto3();
     std::cout << "%TEST_FINISHED% time=0 testLaberinto3 (PbsLaberinto)" << std::endl;
-    
-    std::cout << "%TEST_STARTED% testXstVrt (PbsLaberinto)" << std::endl;
-    testXstVrt();
-    std::cout << "%TEST_FINISHED% time=0 testXstVrt (PbsLaberinto)" << std::endl;
-    
-    std::cout << "%TEST_STARTED% testXstAdy (PbsLaberinto)" << std::endl;
-    testXstAdy();
-    std::cout << "%TEST_FINISHED% time=0 testXstAdy (PbsLaberinto)" << std::endl;
-    
-    std::cout << "%TEST_STARTED% testObtIdVrtInicial (PbsLaberinto)" << std::endl;
-    testObtIdVrtInicial();
-    std::cout << "%TEST_FINISHED% time=0 testObtIdVrtInicial (PbsLaberinto)" << std::endl;
-    
-    std::cout << "%TEST_STARTED% testObtIdVrtFinal (PbsLaberinto)" << std::endl;
-    testObtIdVrtFinal;
-    std::cout << "%TEST_FINISHED% time=0 testObtIdVrtFinal (PbsLaberinto)" << std::endl;
-    
-    std::cout << "%TEST_STARTED% testObtIdVrtAdys (PbsLaberinto)" << std::endl;
-    testObtIdVrtAdys();
-    std::cout << "%TEST_FINISHED% time=0 testObtIdVrtAdys (PbsLaberinto)" << std::endl;
-    
-    std::cout << "%TEST_STARTED% testObtDatoAdy() (PbsLaberinto)" << std::endl;
-    testObtDatoAdy();
-    std::cout << "%TEST_FINISHED% time=0 testObtDatoAdy() (PbsLaberinto)" << std::endl;
-    
-    std::cout << "%TEST_STARTED% testobtCntAdy() (PbsLaberinto)" << std::endl;
-    testObtCntAdy();
-    std::cout << "%TEST_FINISHED% time=0 testobtCntAdy() (PbsLaberinto)" << std::endl;
-    
-    std::cout << "%TEST_STARTED% testObtTotAdy() (PbsLaberinto)" << std::endl;
-    testObtTotAdy();
-    std::cout << "%TEST_FINISHED% time=0 testObtTotAdy() (PbsLaberinto)" << std::endl;
-    
-    std::cout << "%TEST_STARTED% testObtTotVrt (PbsLaberinto)" << std::endl;
-    testObtTotVrt();
-    std::cout << "%TEST_FINISHED% time=0 testObtTotVrt (PbsLaberinto)" << std::endl;
-    
+                 
     std::cout << "%TEST_STARTED% testCaminoMasCorto (PbsLaberinto)" << std::endl;
     testCaminoMasCorto();
     std::cout << "%TEST_FINISHED% time=0 testCaminoMasCorto (PbsLaberinto)" << std::endl;
@@ -174,33 +110,8 @@ int main(int argc, char** argv) {
     std::cout << "%TEST_STARTED% testSumaTotalFerormona (PbsLaberinto)" << std::endl;
     testSumaTotalFerormona();
     std::cout << "%TEST_FINISHED% time=0 testSumaTotalFerormona (PbsLaberinto)" << std::endl;
-    
-    std::cout << "%TEST_STARTED% testAsgIdVrtInicial (PbsLaberinto)" << std::endl;
-    testAsgIdVrtInicial();
-    std::cout << "%TEST_FINISHED% time=0 testAsgIdVrtInicial (PbsLaberinto)" << std::endl;
-    
-    std::cout << "%TEST_STARTED% testAsgIdVrtFinal (PbsLaberinto)" << std::endl;
-    testAsgIdVrtFinal();
-    std::cout << "%TEST_FINISHED% time=0 testAsgIdVrtFinal (PbsLaberinto)" << std::endl;
-    
-    std::cout << "%TEST_STARTED% testAsgDatoAdy (PbsLaberinto)" << std::endl;
-    testAsgDatoAdy();
-    std::cout << "%TEST_FINISHED% time=0 testAsgDatoAdy (PbsLaberinto)" << std::endl;
-    
-    std::cout << "%TEST_STARTED% testDecrementarFerormonaAdys (PbsLaberinto)" << std::endl;
-    testDecrementarFerormonaAdys();
-    std::cout << "%TEST_FINISHED% time=0 testDecrementarFerormonaAdys (PbsLaberinto)" << std::endl;
-    
-    std::cout << "%TEST_STARTED% testActualizarValoracionAdys (PbsLaberinto)" << std::endl;
-    testActualizarValoracionAdys();
-    std::cout << "%TEST_FINISHED% time=0 testActualizarValoracionAdys (PbsLaberinto)" << std::endl;
-    
-    std::cout << "%TEST_STARTED% testObtIndiceAdy (PbsLaberinto)" << std::endl;
-    testObtIndiceAdy();
-    std::cout << "%TEST_FINISHED% time=0 testObtIndiceAdy (PbsLaberinto)" << std::endl;
-    
+              
     std::cout << "%SUITE_FINISHED% time=0" << std::endl;
 
     return (EXIT_SUCCESS);
 }
-
